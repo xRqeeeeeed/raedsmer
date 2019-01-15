bcd124d7051229cb6deb592aabb6b5e268a99432
xRqeeeeeed <46226382+xRqeeeeeed@users.noreply.github.com>
2019-01-15 22:25:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
-var prefix = "A"
+var prefix = "R"
 
 
 
 client.on('message', function(message) {
-    const myID = "462038057529507841";
+    const myID = "529081729642790935";
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "sn")) {
                 if(message.author.id !== myID) return;
